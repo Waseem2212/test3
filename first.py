c97b494839eb0305c51ef9a3c3211099d1255da1
Waseem2212 <muhammadwaseem.study@gmail.com>
2024-10-08 16:23:55 +0500

    add first.py file in github

---
+++ first.py
@@ -0,0 +1,49 @@
+# a = 1
+# # b = 'ali'
+# # c = 4.5
+# # d = 450000
+# # e = False
+
+
+
+# # # this in input
+# # f = input('enter any number')
+# # print('this is the output of the above num', f)
+
+
+# """
+# this is my first comment of the week
+# heello form the axix
+# """
+
+# # type casting
+# _a = int(c)
+
+# """
+# for string string(
+# for float flot()
+# """
+# # for checking data type
+# # print(type(a))
+
+
+# # camel casing
+# myFirstNum = 'waseem'
+# # pascal casing
+# MyFirstNum = 'waseem'
+
+# # snake casing
+# my_first_num = 'waseem'
+# my_name = "Waseem,tamoor"
+# # print(my_name)
+# name_list = print(my_name.split(','))
+# # print(name_list[1])
+# print(len(my_name))
+# if 'waseem' in my_name.lower():
+#     print('yes found')
+# else:
+#     print('no found')
+
+
+my_name = "Waseem,tamoor"
+print(my_name[3:5])
